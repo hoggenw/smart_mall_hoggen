21e69cdfd2dfa429f521b181703c83bc662654a5
wanglg <253192463@qq.com>
2019-02-19 10:29:03 +0800

    no message

---
+++ Smart-Mall/smartMall/src/test/java/hoggen/top/smartMall/SmartMallApplicationTests.java
@@ -0,0 +1,16 @@
+package hoggen.top.smartMall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartMallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ Smart-Mall/小程序/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  version: "1.0",
+  note: '优化接口调用流程',
+  subDomain: "hoggentop", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+  appid: "wx2b98814d18cbc17e", // 您的小程序的appid，购物单功能需要使用
+  shareProfile: '智慧小店，服务为您' // 首页转发的时候话术
+}
